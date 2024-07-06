d9c5d9bd2971a5dcfe068baa45a71bb06712c0e3
Jennifer Zhan <jennifer.zhan.2015@gmail.com>
2024-07-05 22:31:11 -0400

    clear the code add comments

--- Instructions.md
+++ Instructions.md
@@ -1,33 +1,31 @@
-## 6 hours Hackathon
+# 6 hours Hackathon
 
-# Project Planning (time slot 10 AM - 11 AM)
+# Project Planning (10 AM - 11 AM)
 
 // 1. go through 10 APIs find out the most interested in
 // 2. team discuss and decide on an idea
 // 3. define roles and tasks
 
-# Project Execution (time slot)
+# Project Execution (11AM - 3:00PM)
 
-// 1. open a github ? google meeting ? not work in isolation
+// 1. open a github, use zoom or google meeting, not work in isolation
 // 2. building code and check each others blockers
 
-# Middle Check-In (time slot 12PM TA(3-groups) check-in 15mins)
+# Middle Check-In (12PM TA/3-groups)
 
 // Team standup decide any necessary adjustment to complete the project as a team
 
-# Final Check-In (time slot TA final check-in 2PM?)
+# Final Check-In (2PM)
 
 // 1. plan 30mins-60mins to polish work and presentation
 // 2. Avoid last-minute changes!
 // 3. Decide who will share the screen
 // 4. Decide members speak order and which part to introduce
 
-# 3:00-3:30 PM Final Check-In??
-
-# 4:00PM, 4 mins presentation Must include(check list)
+# 4:00PM, 4 mins presentation Must include:
 
 // 1. Intro each members by yourself, Name of the project, 2-3 sentence explanation the purpose of the app, the big idea behind is....and this is we build today ....
 // 2. 1-2 highlights most proud of
 // 3. 1-2 challenges faced during the development
 // 4. Thank you for all the members efforts and eduction team?
-// 5. Four slides? what we did , how we did it? what's the challenge? what's the solution?
+// 5. Four slides: what we did? how we did it? what's the challenge? what's the next?
--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-async function getVehicles() {
+/** Global Variables */
+const vehicles = {
+  A: {
+    name: "",
+    speed: 0,
+  },
+  B: {
+    name: "",
+    speed: 0,
+  },
+};
+
+/** API Calls */
+
+/**
+ * Fetch all vehicles from the API.
+ *
+ * @returns API results
+ */
+async function getAllVehicles() {
   try {
     const response = await axios.get("https://swapi.dev/api/vehicles/");
     return response.data.results;
@@ -7,119 +26,105 @@ async function getVehicles() {
   }
 }
 
-async function randomVehicle(placeholder) {
-  const vehicleArray = await getVehicles();
-  return vehicleArray[placeholder];
-  //   const randomIndex = Math.floor(Math.random() * 10);
-  //   return vehicleArray[randomIndex];
-}
+/** Vehicles */
 
-randomVehicle();
+/**
+ * Select a random vehicle.
+ *
+ * @returns A random vehicle
+ */
+const randomVehicle = (allVehicles) => {
+  const index = Math.floor(Math.random() * allVehicles.length);
+  return allVehicles[index];
+};
 
-async function renderCard() {
+/**
+ * Select two random vehicles and save to global scope.
+ */
+const getRandomVehicles = (allVehicles) => {
+  // get two random vehicles
+  const vehicleA = randomVehicle(allVehicles);
+  let vehicleB = randomVehicle(allVehicles);
+  while (vehicleA.name === vehicleB.name) vehicleB = randomVehicle(vehicles);
+
+  vehicles.A.name = vehicleA.name;
+  vehicles.B.name = vehicleB.name;
+  vehicles.A.speed = parseInt(vehicleA.max_atmosphering_speed);
+  vehicles.B.speed = parseInt(vehicleB.max_atmosphering_speed);
+};
+
+/**
+ * Display two randomly selected vehicles.
+ */
+const renderVehicles = () => {
   const card1 = document.querySelector(".option__button--a");
-  const a = Math.floor(Math.random() * 4) + 1;
-  const resultA = await randomVehicle(a);
-  card1.textContent = resultA.name;
-  const vehicleA = resultA.name;
-
   const card2 = document.querySelector(".option__button--b");
-  const b = Math.floor(Math.random() * 5) + 4;
-  const resultB = await randomVehicle(b);
-  card2.textContent = resultB.name;
-  const vehicleB = resultB.name;
-
-  //   while (vehicleA === vehicleB) {
-  //     resultB = await randomVehicle();
-  //     card2.textContent = resultB.name;
-  //     vehicleB = resultB.name;
-  //   }
+  card1.textContent = vehicles.A.name;
+  card2.textContent = vehicles.B.name;
 
   const cardImageA = document.querySelector(".card__image--a");
   const cardImageB = document.querySelector(".card__image--b");
   cardImageA.setAttribute(
     "src",
-    `./assets/images/${vehicleA.replace(/\//g, " ")}.jpeg`
+    `./assets/images/${vehicles.A.name.replace(/\//g, " ")}.jpeg`
   );
   cardImageB.setAttribute(
     "src",
-    `./assets/images/${vehicleB.replace(/\//g, " ")}.jpeg`
+    `./assets/images/${vehicles.B.name.replace(/\//g, " ")}.jpeg`
   );
+};
 
-  const vehicleASpeed = resultA.max_atmosphering_speed;
-  console.log(vehicleASpeed);
+/** Check Answer */
 
-  const vehicleBSpeed = resultB.max_atmosphering_speed;
-  console.log(vehicleBSpeed);
+/**
+ * Check if user's answer is correct or not and render messages accordingly.
+ *
+ * @param {String} vehicle The answer user selected.
+ */
 
-  const speeds = [
-    {
-      name: vehicleA,
-      speed: vehicleASpeed,
-    },
-    {
-      name: vehicleB,
-      speed: vehicleBSpeed,
-    },
-  ];
-  return speeds;
-}
+// let { nameA, speedA } = vehicles[0];
+// let { nameB, speedB } = vehicles[1];
 
-async function checkAnswer() {
-  const vehicleSpeeds = await renderCard();
-  let answer;
-  if (Number(vehicleSpeeds[0].speed) > Number(vehicleSpeeds[1].speed)) {
-    answer = vehicleSpeeds[0].name;
-  } else {
-    answer = vehicleSpeeds[1].name;
-  }
+const checkAnswer = (vehicle) => {
+  const userAnswer = Object.values(vehicles).filter(
+    (v) => v.name === vehicle
+  )[0].speed;
 
-  const buttonA = document.querySelector(".option__button--a");
-  const buttonB = document.querySelector(".option__button--b");
+  console.log(vehicles.A.speed, vehicles.B.speed);
+  const correct = userAnswer === Math.max(vehicles.A.speed, vehicles.B.speed);
+
   const answerText = document.querySelector(".answer__text");
+  if (correct) {
+    answerText.textContent = `Correct! 🔥 ${vehicle} is faster! It max speed is ${userAnswer}. May the force be with you!`;
+    answerText.classList.remove("answer__text--incorrect");
+    answerText.classList.add("answer__text--correct");
+  } else {
+    answerText.textContent = `Wrong! ☠️ ${vehicle}'s max speed is ${userAnswer}. Best luck next time!`;
+    answerText.classList.remove("answer__text--correct");
+    answerText.classList.add("answer__text--incorrect");
+  }
+};
 
-  buttonA.addEventListener("click", (event) => {
-    const correctClass = document.querySelector(".answer__text--correct");
-    const incorrectClass = document.querySelector(".answer__text--incorrect");
-    if (correctClass) {
-      answerText.classList.remove("answer__text--correct");
-    }
-    if (incorrectClass) {
-      answerText.classList.remove("answer__text--incorrect");
-    }
+/** HTML/Attach event listeners */
+const buttonA = document.querySelector(".option__button--a");
+const buttonB = document.querySelector(".option__button--b");
 
-    // console.log(rightAnswer());
-    if (event.target.textContent === answer) {
-      answerText.textContent = "Correct! 🔥  May the force be with you";
-      answerText.classList.add("answer__text--correct");
-    } else {
-      answerText.textContent = "Wrong! ☠️ Best luck next time";
-      answerText.classList.add("answer__text--incorrect");
-    }
-  });
-
-  buttonB.addEventListener("click", (event) => {
-    const correctClass = document.querySelector(".answer__text--correct");
-    const incorrectClass = document.querySelector(".answer__text--incorrect");
-    if (correctClass) {
-      answerText.classList.remove("answer__text--correct");
-    }
-    if (incorrectClass) {
-      answerText.classList.remove("answer__text--incorrect");
-    }
-    // console.log(rightAnswer());
-    if (event.target.textContent === answer) {
-      answerText.textContent = "Correct! 🔥 May the force be with you";
-      answerText.classList.add("answer__text--correct");
-    } else {
-      answerText.textContent = "Wrong! ☠️ Best luck next time";
-      answerText.classList.add("answer__text--incorrect");
-    }
-  });
-}
-checkAnswer();
+buttonA.addEventListener("click", () => checkAnswer(buttonA.textContent));
+buttonB.addEventListener("click", () => checkAnswer(buttonB.textContent));
 
 const tryAgain = document.querySelector(".againButton");
 tryAgain.addEventListener("click", () => {
   location.reload();
 });
+
+/** Entrypoint */
+const main = async () => {
+  // download all vehicles through SW API
+  const allVehicles = await getAllVehicles();
+  // select two vehicles randomly
+  getRandomVehicles(allVehicles);
+  // display vehicles
+  renderVehicles();
+};
+
+main();
